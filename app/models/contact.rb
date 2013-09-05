d2d1d39de6b7795b0cc64004017d61dba0fdb99b
James Robey <james.robey@gmail.com>
2013-09-05 16:09:30 -0700

    contactus/ form added, integrated to mailer

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,8 @@
 class Contact < ActiveRecord::Base
   attr_accessible :body, :email, :name, :subject
+
+  validates :email, :body, presence: true 
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
+
+
 end
