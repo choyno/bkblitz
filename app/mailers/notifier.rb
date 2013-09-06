04f5540229bc1054d581e5c22b8efef5619a94af
James Robey <james.robey@gmail.com>
2013-09-05 17:01:31 -0700

    got tests working

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,9 +1,10 @@
 class Notifier < ActionMailer::Base
   default from: "from@example.com"
+  default to: "contact@blitzgift.com"
 
   def new_contact(contact)
     @contact = contact
-    mail(:subject => contact.subject)
+    mail(:to => "contact@blitzgift.com", :subject => contact.subject)
   end
 
 
--- spec/features/contact_us_spec.rb
+++ spec/features/contact_us_spec.rb
@@ -9,21 +9,24 @@ feature 'Contact Us mail form' do
     fill_in :email, :with => ''
     click_button 'Send'
     
-    page.errors.on(:email).should == 'is required'
+    #page.errors.on(:email).should == 'is required'
+    expect(page).to have_content "Email can't be blank" 
   end
   
   scenario 'with invalid email' do
     fill_in :email, :with => '@invalid'
     click_button 'Send'
     
-    page.errors.on(:email).should == 'is not valid'
+    #page.errors.on(:email).should == 'is not valid'
+    expect(page).to have_content "Email is invalid"
   end
   
   scenario 'with blank body' do
     fill_in :body, :with => ''
     click_button 'Send'
     
-    page.errors.on(:body).should == 'is required'
+    #page.errors.on(:body).should == 'is required'
+    expect(page).to have_content "Body can't be blank" 
   end
   
   scenario 'with valid email and body' do
@@ -31,7 +34,10 @@ feature 'Contact Us mail form' do
     fill_in :body, :with => 'This is an automated test message.'
     click_button 'Send'
       
-    page.should have_sent_email.to('contact@blitzgift.com')
-    page.should have_content('sent')
+    #page.should have_sent_email.to('contact@blitzgift.com')
+    #page.should have_content('sent')
+    email = ActionMailer::Base.deliveries.last
+    expect(email.to).to have_content('contact@blitzgift.com')
+    expect(page).to have_content "Email was successfully sent."
   end
 end
