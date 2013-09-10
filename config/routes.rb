cb11002290f6bbfb044ae0ebb8011c800bb1a09e
Michael Puterbaugh <mputerba@gmail.com>
2013-09-10 11:06:53 -0400

    Update Devise setup to remove deprecation warning

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Blitzgift::Application.routes.draw do
+  root to: 'contacts#index'
+
   resources :contacts
   match 'contactus' => 'contacts#new', :as => :contactus
-  
-  root to: 'users#show'
 
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', registration: 'users' } do
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', registration: 'users' } 
+  devise_scope :user do
     get '/register', controller: 'devise/registrations', action: :new, as: :new_user_registration
   end
 
   get '/myaccount', controller: :users, action: :show
-
 end
